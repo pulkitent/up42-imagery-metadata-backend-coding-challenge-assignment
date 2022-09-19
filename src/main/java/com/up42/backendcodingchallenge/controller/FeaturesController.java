f0c03ca1cabc10792a69ac4687ae6e01c62f1630
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 12:12:09 +0530

    Add new package named controller & moved FeaturesController into it

--- src/main/java/com/up42/backendcodingchallenge/controller/FeaturesController.java
+++ src/main/java/com/up42/backendcodingchallenge/controller/FeaturesController.java
@@ -1,4 +1,4 @@
-package com.up42.backendcodingchallenge;
+package com.up42.backendcodingchallenge.controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
