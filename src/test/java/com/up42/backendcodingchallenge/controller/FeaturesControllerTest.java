7f320be9e319dd614fbc49e5b81a6d56398bec4f
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 14:07:40 +0530

    Add unit test for FeatureController named FeatureControllerTest

---
+++ src/test/java/com/up42/backendcodingchallenge/controller/FeaturesControllerTest.java
@@ -0,0 +1,63 @@
+package com.up42.backendcodingchallenge.controller;
+
+import com.up42.backendcodingchallenge.dto.FeatureDTO;
+import com.up42.backendcodingchallenge.service.FeatureService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FeatureControllerTest {
+  @InjectMocks
+  private FeatureController featureController;
+  @Mock
+  private FeatureService featureService;
+
+  @Test
+  @DisplayName("Test getFeatures should return HTTP 200 with feature list having one feature")
+  void testGetFeatures_ShouldReturnHttpOkWithOneFeature() {
+    // Arrange
+    FeatureDTO featureDTO = new FeatureDTO();
+    featureDTO.setPropertyId(UUID.randomUUID());
+    featureDTO.setPropertyTimestamp(12345678L);
+    featureDTO.setPropertyAcquisitionMissionName("MissionName");
+    featureDTO.setPropertyAcquisitionEndViewingDate(234567L);
+    featureDTO.setPropertyAcquisitionBeginViewingDate(4576790L);
+    List<FeatureDTO> featureDTOS = Collections.singletonList(featureDTO);
+    Mockito.when(featureService.getAllFeatures()).thenReturn(featureDTOS);
+
+    // Act
+    ResponseEntity<List<FeatureDTO>> featuresResponse = featureController.getFeatures();
+
+    // Assert
+    assertEquals(HttpStatus.OK, featuresResponse.getStatusCode());
+    assertEquals(featureDTOS, featuresResponse.getBody());
+  }
+
+  @Test
+  @DisplayName("Test getFeatures should return HTTP 200 with empty feature list when features are not present")
+  void testGetFeatures_ShouldReturnHttpOkWithEmptyList() {
+    // Arrange
+    Mockito.when(featureService.getAllFeatures()).thenReturn(Collections.EMPTY_LIST);
+
+    // Act
+    ResponseEntity<List<FeatureDTO>> featuresResponse = featureController.getFeatures();
+
+    // Assert
+    assertEquals(HttpStatus.OK, featuresResponse.getStatusCode());
+    assertNotNull(featuresResponse.getBody());
+    assertEquals(0, featuresResponse.getBody().size());
+  }
+}
