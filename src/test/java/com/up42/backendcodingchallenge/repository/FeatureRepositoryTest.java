42ff1ee2c70bc1ab86105b4c3b26866ab83a6ce3
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 14:15:02 +0530

    Add a basic unit test for FeaturesRepository named FeaturesRepositoryTest

---
+++ src/test/java/com/up42/backendcodingchallenge/repository/FeatureRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.up42.backendcodingchallenge.repository;
+
+import com.up42.backendcodingchallenge.model.Feature;
+import com.up42.backendcodingchallenge.model.FeatureCollection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class FeaturesRepositoryTest {
+  @Test
+  @DisplayName("Test findAll should return non empty feature collection list when features are present")
+  void testFindAll_ShouldReturnNonEmptyFeatureCollectionList() {
+    // Arrange
+    Feature feature = new Feature(null);
+    FeatureRepository featuresRepository = new FeatureRepository("classpath:static/source-data.json");
+
+    // Act
+    List<FeatureCollection> featureCollections = featuresRepository.findAll();
+
+    // Assert
+    assertFalse(featureCollections.isEmpty());
+  }
+}
