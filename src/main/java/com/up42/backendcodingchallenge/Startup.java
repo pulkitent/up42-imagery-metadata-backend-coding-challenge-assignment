bc627a1e88fe7cbc3343b0a96dcff7d40285547f
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 14:20:17 +0530

    Renamed file BackendCodingChallengeApplication.java to Startup.java

--- src/main/java/com/up42/backendcodingchallenge/Startup.java
+++ src/main/java/com/up42/backendcodingchallenge/Startup.java
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
-public class BackendCodingChallengeApplication {
+public class Startup {
 
   public static void main(String[] args) {
-    SpringApplication.run(BackendCodingChallengeApplication.class, args);
+    SpringApplication.run(Startup.class, args);
   }
 }
