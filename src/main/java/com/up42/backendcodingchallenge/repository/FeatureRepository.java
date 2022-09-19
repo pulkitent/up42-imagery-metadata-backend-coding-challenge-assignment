36b1a8798604abc3d160761bfdf1985fb315c89c
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 13:50:30 +0530

    Refactored the code to pick static data source file path from application.properties so as to make it configurable

--- src/main/java/com/up42/backendcodingchallenge/repository/FeatureRepository.java
+++ src/main/java/com/up42/backendcodingchallenge/repository/FeatureRepository.java
@@ -3,6 +3,8 @@ package com.up42.backendcodingchallenge.repository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.up42.backendcodingchallenge.model.FeatureCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.ResourceUtils;
 
@@ -12,11 +14,18 @@ import java.util.List;
 
 @Repository
 public class FeatureRepository {
+  private final String filePath;
+
   private static final ObjectMapper mapper = new ObjectMapper();
+
+  @Autowired
+  public FeatureRepository(@Value("${source.data.file.path}") final String filePath) {
+    this.filePath = filePath;
+  }
 
   public List<FeatureCollection> findAll() throws IOException {
     try {
-      final File file = ResourceUtils.getFile("classpath:static/source-data.json");
+      final File file = ResourceUtils.getFile(filePath);
       return mapper.readValue(file, new TypeReference<List<FeatureCollection>>() {
       });
     } catch (IOException ex) {
