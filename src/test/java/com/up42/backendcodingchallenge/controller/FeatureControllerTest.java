5f6ffd947137c372583c6f652ccff46f831a65f1
Pulkit Gupta <pulkit.gupta@gojek.com>
2022-09-19 16:05:03 +0530

    Add controller test for getQuicklook()

--- src/test/java/com/up42/backendcodingchallenge/controller/FeatureControllerTest.java
+++ src/test/java/com/up42/backendcodingchallenge/controller/FeatureControllerTest.java
@@ -10,8 +10,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -59,5 +62,41 @@ class FeatureControllerTest {
     assertEquals(HttpStatus.OK, featuresResponse.getStatusCode());
     assertNotNull(featuresResponse.getBody());
     assertEquals(0, featuresResponse.getBody().size());
+  }
+
+  @Test
+  @DisplayName("Test quickLook should return HTTP 200 with decoded quicklook when quicklook is present for given featureId")
+  void testGetQuicklook_ShouldReturnHttpOkWithDecodedQuickLookWhenQuickLookIsPresent() {
+    // Arrange
+    UUID uuid = UUID.randomUUID();
+    String encodedString = "cHVsa2l0";
+    byte[] expectedDecodedQuickLook = Base64.getDecoder().decode(encodedString.getBytes(StandardCharsets.UTF_8));
+    Mockito.when(featureService.getQuickLookByFeatureId(uuid)).thenReturn(expectedDecodedQuickLook);
+
+    // Act
+    ResponseEntity<byte[]> decodedQuickLookResponse = featureController.getQuicklook(uuid);
+
+    // Assert
+    assertEquals(HttpStatus.OK, decodedQuickLookResponse.getStatusCode());
+    assertNotNull(decodedQuickLookResponse.getHeaders());
+    assertEquals(MediaType.IMAGE_PNG, decodedQuickLookResponse.getHeaders().getContentType());
+    assertEquals(expectedDecodedQuickLook, decodedQuickLookResponse.getBody());
+  }
+
+  @Test
+  @DisplayName("Test quickLook should return HTTP 404 with no response body when featureId is not found")
+  void testGetQuicklook_ShouldReturnHttpNotFoundWithEmptyResponseBodyWhenFeatureIdIsNotPresent() {
+    // Arrange
+    UUID uuid = UUID.randomUUID();
+    Mockito.when(featureService.getQuickLookByFeatureId(uuid)).thenReturn(new byte[0]);
+
+    // Act
+    ResponseEntity<byte[]> decodedQuickLookResponse = featureController.getQuicklook(uuid);
+
+    // Assert
+    assertEquals(HttpStatus.NOT_FOUND, decodedQuickLookResponse.getStatusCode());
+    assertNotNull(decodedQuickLookResponse.getHeaders());
+    assertEquals(MediaType.IMAGE_PNG, decodedQuickLookResponse.getHeaders().getContentType());
+    assertNull(decodedQuickLookResponse.getBody());
   }
 }
